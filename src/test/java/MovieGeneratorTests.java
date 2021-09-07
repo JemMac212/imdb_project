417f14d698479d33aeb6346f89eb4ea79b741b83
jemima.mackenzie <jemima.mackenzie@fanduel.com>
2021-09-07 12:17:27 +0100

    Updated

--- src/test/java/MovieGeneratorTests.java
+++ src/test/java/MovieGeneratorTests.java
@@ -1,4 +1,4 @@
-/*
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -6,31 +6,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MovieGeneratorTests {
-    private static Movie randomMovie;
-    private static Map<String, Movie> imdbMap;
 
     @BeforeAll
     public static void setup(){
-        imdbMap = new MovieGenerator().generateImdbMap();
-        List<String[]> imdbRawData = new MovieRecordScanner().generateRawImdbData();
-        Random random = new Random();
-        randomMovie = new Movie(imdbRawData.get(random.nextInt(imdbRawData.size())));
     }
 
     @Test
     public void imdbTests(){
-        assertEquals(randomMovie.getTitle(), imdbMap.get(randomMovie.getYear()));
     }
 }
 
-*/
-/* public static void main(String[]args){
-    System.out.println(randomImdb.getTitle());
-}
-*/
